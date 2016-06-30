124d64203bdcb6fcfe0861b743eea809471ab7ad
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-06-30 15:35:28 -0700

    Added user adding & removing books

--- routes/session.js
+++ routes/session.js
@@ -2,7 +2,49 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
 
-// YOUR CODE HERE
+router.post('/session', (req, res, next) => {
+  knex('users')
+    .where('email', req.body.email)
+    .first()
+    .then((user) => {
+      if (!user) {
+        return res
+          .set('Content-Type', 'text/plain')
+          .sendStatus(401);
+      }
+
+      const hashed_password = user.hashed_password;
+
+      bcrypt.compare(req.body.password, hashed_password, (err, isMatch) => {
+        if (err) {
+          next(err);
+        }
+
+        if (!isMatch) {
+          // User password was incorrect
+          return res
+            .set('Content-Type', 'text/plain')
+            .sendStatus(401);
+        }
+
+        // User is authenticated
+        req.session.user = user;
+        res.cookie('loggedIn', true);
+        res.sendStatus(200);
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/session', (req, res, next) => {
+  req.session = null;
+  res.clearCookie('loggedIn');
+  res.sendStatus(200);
+});
 
 module.exports = router;
--- routes/users_books.js
+++ routes/users_books.js
@@ -2,7 +2,110 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
 
-// YOUR CODE HERE
+const checkAuth = function(req, res, next) {
+  if (!req.session.user) {
+    return res.sendStatus(401);
+  }
+
+  next();
+}
+
+router.get('/users/books', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+
+  knex('users_books')
+    .innerJoin('books', 'users_books.book_id', 'books.id')
+    .where('users_books.user_id', userId)
+    .then((books) => {
+      if (!books) {
+        return next();
+      }
+
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  if (Number.isNaN(bookId)) {
+    return next();
+  }
+  knex('books')
+    .innerJoin('users_books', 'users_books.book_id', 'books.id')
+    .where({
+      'users_books.user_id': userId,
+      'users_books.book_id': bookId
+    })
+    .then((results) => {
+      if (!results[0]) {
+        return next();
+      }
+
+      res.send(results[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  if (Number.isNaN(bookId)) {
+    return next();
+  }
+
+  knex('users_books')
+    .insert({
+      user_id: userId,
+      book_id: bookId
+    }, '*')
+    .then((results) => {
+      res.send(results[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  if (Number.isNaN(bookId)) {
+    return next();
+  }
+
+  knex('users_books')
+    .where('user_id', userId)
+    .where('book_id', bookId)
+    .first()
+    .then((usersBook) => {
+      if (!usersBook) {
+        return next();
+      }
+
+      return knex('users_books')
+        .del()
+        .where('user_id', userId)
+        .where('book_id', bookId)
+        .then(() => {
+          delete usersBook.id;
+          res.send(usersBook);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
